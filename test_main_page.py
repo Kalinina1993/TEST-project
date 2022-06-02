863ca2a16c43b69e322ed63edc8c107ae0ddc089
Kalinina93 <www.queenherebitches@mail.ru>
2022-06-02 12:00:25 +0300

    Changes in test and pages files

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,8 @@
-from selenium.webdriver.common.by import By
+from pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     url = "http://selenium1py.pythonanywhere.com/"
-    browser.get(url)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, url)
+    page.open()
+    page.do_to_login_page()
